f8251c7cfb3c25dcf1153ee42e9b255377a100c2
ZmicerOk <vasilkou@73gmail.com>
2019-02-08 01:37:42 +0300

    clear func

--- redux-sandbox-my/src/index.js
+++ redux-sandbox-my/src/index.js
@@ -1,1 +1,21 @@
-console.log("123test");
+import {createStore} from "redux";
+
+// const initialState = 0;
+
+const reducer = (state=0, action) => {
+    switch (action.type) {
+        case "INC": return state+1;
+        case "DEC": return state-1;
+        default: return state;
+    }
+};
+
+const store = createStore(reducer);
+store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+store.dispatch({type:"INC"});
+store.dispatch({type:"INC"});
+store.dispatch({type:"INC"});
+store.dispatch({type:"INC"});
