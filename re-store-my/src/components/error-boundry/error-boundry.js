c9853bae3addd369f747244eed4100abaa272018
ZmicerOk <vasilkou@73gmail.com>
2019-02-18 13:51:28 +0300

    125 ok

--- re-store-my/src/components/error-boundry/error-boundry.js
+++ re-store-my/src/components/error-boundry/error-boundry.js
@@ -11,7 +11,7 @@ export default class ErrorBoundry extends Component{
     };
 
     render(){
-        if(this.statr.hasError){
+        if(this.state.hasError){
             return <ErrorIndicator/>;
         }
         return this.props.children;
--- re-store/30-connecting-components/src/components/pages/home-page.js
+++ re-store/30-connecting-components/src/components/pages/home-page.js
@@ -3,7 +3,7 @@ import BookList from '../book-list';
 
 const HomePage = () => {
   return (
-    <BookList />
+    <BookList books={[]}/>
   );
 };
 
